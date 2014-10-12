57b0933c16a10685fca95f246a5ee9deafe61176
Hanif Jameel <hanif.jameel@btinternet.com>
2014-10-12 16:44:38 +0100

    Extract a csv for each table in the html.

--- flashcardgenerate.py
+++
@@ -1,7 +0,0 @@
-from BeautifulSoup import BeautifulSoup
-from urllib2 import urlopen
-
-import sys
-
-html = urlopen(sys.argv[1]).read()
-print html
---
+++ tableextractor.py
@@ -0,0 +1,52 @@
+import cStringIO
+import codecs
+import csv
+import sys
+
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+def parse_html_from_url(url):
+    html = urlopen(url).read()
+    return BeautifulSoup(html)
+
+def write_csv_for_each_table_in_html(html):
+    for i, table in enumerate(html("table")):
+        with open("{}.csv".format(i), "wb") as csvfile:
+            writer = UnicodeWriter(csvfile)
+            for row in table("tr"):
+                writer.writerow([column.text for column in row("td")])
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    html = parse_html_from_url(url)
+    write_csv_for_each_table_in_html(html)
