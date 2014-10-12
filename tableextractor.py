a95a1600ed17f58926ba1e112a26db14dee02805
Hanif Jameel <hanif.jameel@btinternet.com>
2014-10-12 16:55:55 +0100

    Change the name of the output CSV files.

--- tableextractor.py
+++ tableextractor.py
@@ -41,7 +41,7 @@ def parse_html_from_url(url):
 
 def write_csv_for_each_table_in_html(html):
     for i, table in enumerate(html("table")):
-        with open("{}.csv".format(i), "wb") as csvfile:
+        with open("table_{}.csv".format(i), "wb") as csvfile:
             writer = UnicodeWriter(csvfile)
             for row in table("tr"):
                 writer.writerow([column.text for column in row("td")])
